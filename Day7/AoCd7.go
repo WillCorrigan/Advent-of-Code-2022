e16bfad08b8fc9a9f1f46e4d8c1b674df1933ba3
Will <wcorrigan90@gmail.com>
2022-12-08 21:21:06 +0000

    d7 part 2

--- Day7/AoCd7.go
+++ Day7/AoCd7.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	Part1()
-	// Part2()
+	Part2()
 }
 
 type Directory struct {
@@ -18,6 +18,26 @@ type Directory struct {
 	ParentDirectory   *Directory
 	Files             []int
 	NestedDirectories []*Directory
+}
+
+func RecursionThroughFolders(folderStructure map[string]*Directory, pwd *Directory) int {
+	totalToReturn := 0
+	totalToReturn += Sum(pwd.Files)
+	for _, dir := range pwd.NestedDirectories {
+		totalToReturn += RecursionThroughFolders(folderStructure, folderStructure[dir.Name])
+	}
+	return totalToReturn
+}
+
+func Sum(arr []int) int {
+	total := 0
+	if len(arr) == 0 {
+		return 0
+	}
+	for _, v := range arr {
+		total += v
+	}
+	return total
 }
 
 func Part1() {
@@ -83,26 +103,86 @@ func Part1() {
 	fmt.Println(total)
 }
 
-func RecursionThroughFolders(folderStructure map[string]*Directory, pwd *Directory) int {
+func Part2() {
+	inputResult := loadTextInput.LoadInput("input.txt")
+
+	parse := strings.Split(inputResult, "\r\n")
+
+	currentWorkingDirectory := &Directory{
+		Name:              "/",
+		Files:             nil,
+		NestedDirectories: nil,
+		ParentDirectory:   nil}
+
+	mapOfFolders := make(map[string]*Directory)
+
+	mapOfFolders[currentWorkingDirectory.Name] = currentWorkingDirectory
+
+	for i, v := range parse {
+		if strings.HasPrefix(v, "$ ls") {
+			continue
+		} else if strings.HasPrefix(v, "dir") {
+			if _, exists := mapOfFolders[fmt.Sprintf("%v/%v", currentWorkingDirectory.Name, v[4:])]; exists {
+				currentWorkingDirectory.NestedDirectories =
+					append(currentWorkingDirectory.NestedDirectories,
+						mapOfFolders[fmt.Sprintf("%v/%v", currentWorkingDirectory.Name, v[4:])])
+			} else {
+				newDirectory := &Directory{
+					Name:              fmt.Sprintf("%v/%v", currentWorkingDirectory.Name, v[4:]),
+					Files:             nil,
+					NestedDirectories: nil,
+					ParentDirectory:   currentWorkingDirectory}
+				currentWorkingDirectory.NestedDirectories =
+					append(currentWorkingDirectory.NestedDirectories, newDirectory)
+				mapOfFolders[newDirectory.Name] = newDirectory
+			}
+		} else if strings.HasPrefix(v, "$ cd") {
+			mapOfFolders[currentWorkingDirectory.Name] = currentWorkingDirectory
+			if strings.HasSuffix(v, "..") {
+				currentWorkingDirectory = mapOfFolders[currentWorkingDirectory.ParentDirectory.Name]
+			} else if strings.HasSuffix(v, "/") {
+				currentWorkingDirectory = mapOfFolders["/"]
+			} else {
+				p := fmt.Sprintf("%v/%v", currentWorkingDirectory.Name, v[5:])
+				currentWorkingDirectory = mapOfFolders[p]
+			}
+		} else {
+			x, _ := strconv.Atoi(strings.Split(v, " ")[0])
+			currentWorkingDirectory.Files = append(currentWorkingDirectory.Files, x)
+			if i == len(parse)-1 {
+				mapOfFolders[currentWorkingDirectory.Name] = currentWorkingDirectory
+			}
+		}
+	}
+
+	var fileSizeTotal int
+	var spaceToFree int
+	root := mapOfFolders["/"]
+	fileSizeTotal += RecursionThroughRoot(mapOfFolders, root)
+	fmt.Printf("Total file size: %v\n", fileSizeTotal)
+	spaceToFree = 30000000 - (70000000 - fileSizeTotal)
+	fmt.Printf("Total space needed to free: %v\n", spaceToFree)
+
+	folderSizes := make(map[string]int)
+	for _, folder := range mapOfFolders {
+		fileSizeTotal := 0
+		fileSizeTotal += RecursionThroughFolders(mapOfFolders, folder)
+		folderSizes[folder.Name] = fileSizeTotal
+	}
+	minSizeOfFolderToDelete := fileSizeTotal
+	for _, val := range folderSizes {
+		if val >= spaceToFree && val < minSizeOfFolderToDelete {
+			minSizeOfFolderToDelete = val
+		}
+	}
+	fmt.Printf("Minimum size of folder to delete (part 2): %v\n", minSizeOfFolderToDelete)
+}
+
+func RecursionThroughRoot(mapOfFolders map[string]*Directory, directory *Directory) int {
 	totalToReturn := 0
-	totalToReturn += Sum(pwd.Files)
-	for _, dir := range pwd.NestedDirectories {
-		totalToReturn += RecursionThroughFolders(folderStructure, folderStructure[dir.Name])
+	totalToReturn += Sum(directory.Files)
+	for _, dir := range directory.NestedDirectories {
+		totalToReturn += RecursionThroughRoot(mapOfFolders, mapOfFolders[dir.Name])
 	}
 	return totalToReturn
 }
-
-func Sum(arr []int) int {
-	total := 0
-	if len(arr) == 0 {
-		return 0
-	}
-	for _, v := range arr {
-		total += v
-	}
-	return total
-}
-
-// func Part2() {
-
-// }
